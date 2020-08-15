6bf3383e0c88e687cfbace237af6f8016364e3a5
Mohamed Mojahed <mohamed.mojahead@gmail.com>
2020-08-15 03:35:39 +0300

    side Bar Content and Styleing

--- dist/js/main-min.js
+++ dist/js/main-min.js
@@ -1,1 +1,1 @@
-$(function(){$(".toggle-side").on("click",function(){$(".sidebar, .content-area").toggleClass("no-side")})});
+$(function(){$(".toggle-side").on("click",function(){$(".sidebar, .content-area").toggleClass("no-side")}),$(".toggle").on("click",function(){$(this).next(".child-links").slideToggle(),$(this).find(".fa-angle-right").toggleClass("down")})});
--- stage/js/main.js
+++ stage/js/main.js
@@ -2,4 +2,8 @@ $(function() {
     $(".toggle-side").on("click", function() {
         $(".sidebar, .content-area").toggleClass("no-side")
     })
+    $(".toggle").on("click", function() {
+        $(this).next(".child-links").slideToggle();
+        $(this).find(".fa-angle-right").toggleClass("down")
+    })
 })
