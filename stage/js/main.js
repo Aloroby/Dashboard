cbc7f4778d8baab8b8749e7f25696134fc0f42a9
Mohamed Mojahed <mohamed.mojahead@gmail.com>
2020-08-15 16:47:24 +0300

    Add Box Fonts To Settings To Change Font In Pages

--- stage/js/main.js
+++ stage/js/main.js
@@ -1,30 +1,45 @@
 $(function() {
+    // Open And Close SideBar
     $(".toggle-side").on("click", function() {
         $(".sidebar, .content-area").toggleClass("no-side")
     });
+    // Open And Close Side Bar Links
     $(".toggle").on("click", function() {
         $(this).next(".child-links").slideToggle();
         $(this).find(".fa-angle-right").toggleClass("down")
     });
+    // Full Screen Option
     $(".toggle-screen").on("click", function() {
         $(this).toggleClass("full-screen");
         if ($(this).hasClass("full-screen")) {
-            openFullscreen()
+            openFullscreen();
         } else {
-            closeFullscreen()
+            closeFullscreen();
         }
     });
+    // Open and Close Settings Box
     $(".toggle-box").on("click", function() {
         $(this).find("i").toggleClass("fa-spin");
         $(this).parent().toggleClass("hide-box");
-    })
+    });
+    // Get Classes Of Colors to Remove Him From Tag Body
     var themesColor = []
     $(".color-box li").each(function() {
-        themesColor.push($(this).data("color"))
-    })
+            themesColor.push($(this).data("color"))
+        })
+        // Toggle Color Classes on Body To Change Pages Color
     $(".color-box li").on("click", function() {
-        $("body").removeClass(themesColor.join(" ")).addClass($(this).data("color"))
-        $(this).addClass("active").siblings().removeClass("active")
+            $("body").removeClass(themesColor.join(" ")).addClass($(this).data("color"))
+            $(this).addClass("active").siblings().removeClass("active")
+        })
+        // Get Classes Of Fonts to Remove Him From Tag Body
+    var fontClasses = []
+    $(".fonts-box select option").each(function() {
+            fontClasses.push($(this).val())
+        })
+        // Change Font Of Body
+    $(".fonts-box select").on("change", function() {
+        $("body").removeClass(fontClasses.join(" ")).addClass($(this).find("option:selected").val())
     })
 });
 var elem = document.documentElement;
